5a125b77ac93d4731356d613a94a5b8a7ec4bed6
Matt Holman <mholman@Matts-MacBook-Pro.local>
2016-11-23 13:22:30 -0500

    GHG dictionary

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@ var facilityLocations = [],
     citiesMA = [],
     plants = [],
     ghg = {},
-    GHGsum = 0.0,
-    cnt = 0;
+    GHGsum = {},
+    defaultUSDperKWh = 0.20,
+    metricTonsPerLb = 0.000453592;
 
 // Holden, MA (~center of Massachusetts) -- for center
 var centerOfMA = [42.358734, -71.849239];
@@ -40,6 +41,7 @@ function createVis(error, regionsServed, massCities, plantsData, GHGdata) {
     citiesMA = massCities;
     plants = plantsData;
 
+    // Making the ghg factor dictionary.
     GHGdata.forEach(function(d){
 	ghg[d.FY] = d["GHG factor"];
     });
@@ -56,7 +58,8 @@ function createVis(error, regionsServed, massCities, plantsData, GHGdata) {
 	delete d.SavingsKWh;
     });
 
-    // Computing GHG emissions
+    // Computing GHG emissions from primary data, rather than using
+    // the values in the excel sheets.
     plants.forEach(function(d) {
 	if(d.ElectricityGenerationKWh != ""){
 	    d.GHG = (+d.ElectricityGenerationKWh)*ghg[d.FY];
@@ -65,42 +68,60 @@ function createVis(error, regionsServed, massCities, plantsData, GHGdata) {
 	}
     });
     
-    // Trying to regularized the cost rates
+    // Regularizing the cost rates
     plants.forEach(function(d) {
 
 	// There are a few missing UsageUSD values;
+	// fill them in where possible.
 	if(d.UsageUSD == "" && d.UsageKWh != "" && d.USDperKWh != ""){
 	    d.UsageUSD = (+d.UsageKWh) * (+d.USDperKWh);	    
 	}
-	
+
 	if(d.USDperKWh != ""){
+	    // If there as a rate in the csv file, use it.
 	    d.Rate = (+d.USDperKWh);
 	}else if(d.UsageUSD != "" && d.UsageKWh != ""){
+	    // If the rate is missing in the csv file but we
+	    // have the usage in USD and KWh, compute the rate
 	    d.Rate = (+d.UsageUSD)/(+d.UsageKWh);
 	}else {
-	    cnt++;
-	    d.Rate = (+d.UsageUSD)/(+d.UsageKWh);
+	    // Otherwise, just assume a defaultUSDperKWh;
+	    d.Rate = defaultUSDperKWh;
 	}
     });
 
-   // console.log(plants);
+    console.log(plants);
     var nested = d3.nest()
 	.key(function(d) { return d.Facility;})
     	.key(function(d) { return d.Type;})
 	.entries(plants);
 
-    var metricTonsPerLb = 0.000453592;
     nested.forEach(function(d){
 	d.values.forEach(function(data, index){
+	    GHGsum[d.key] = 0.0;
 	    data.values.forEach(function(d2, i2){
 		if((+d2.FY)>=13){
-		    GHGsum += d2.GHG*metricTonsPerLb;
+		    GHGsum[d.key] += d2.GHG*metricTonsPerLb;
 		}
 	    });
-	    //console.log(d.key, "GHGsum: ", GHGsum);
+	    console.log(d.key, "GHGsum: ", GHGsum[d.key]);
 	});
     });
 
+    for(key in GHGsum){
+	console.log(key, GHGsum[key]);
+    }
+
+    nested.forEach(function(data1){
+	data1.values.forEach(function(data2, index){
+	    data2.values.forEach(function(data3, i2){
+		if(data3.ElectricityGenerationKWh != "" && (+data3.FY)>=10){
+		    console.log(data3.Facility, data3.Type, data3.FY, data3.UsageKWh, data3.ElectricityGenerationKWh, data3.ElectricityGenerationKWh/data3.UsageKWh);
+		}
+	    });
+	});
+    });
+    
     plants.forEach(function(d, index){
 	if(d.ElectricityGenerationKWh != ""){
 	    console.log("plant: ", d.Facility, d.FY, d.UsageKWh, d.ElectricityGenerationKWh);
