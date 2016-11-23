f8acacf1af22185cf9005916ee66d3a8521062ec
Alma Lafler <alafler@college.harvard.edu>
2016-11-23 17:03:10 -0500

    cleaned up main.js

--- js/main.js
+++ js/main.js
@@ -24,6 +24,18 @@ L.Icon.Default.imagePath = 'img/';
 
 
 
+// INSTANTIATE VISUALIZATIONS ------------------------------------------------------------
+
+// create visualizations
+function createVis() {
+    facilityMap = new FacilityMap("facility-map", facilityLocations, citiesMA, centerOfMA);
+    co2Savings = new co2Savings("co2-Savings", GHGsum);
+    usageCostScatter = new UsageCostScatter("usagecost-scatter", plants);
+}
+
+
+
+
 // WORK WITH DATA ------------------------------------------------
 
 // load data asynchronously
@@ -42,113 +54,18 @@ function wrangleData(error, regionsServed, massCities, plantsData, GHGdata) {
         facilityLocations = regionsServed;
         plants = plantsData;
 
-        // Making the ghg factor dictionary.
+        // wrangle "plants" dataset
+        wranglePlants();
+
+        // wrangle "SummaryData" dataset
+        wrangleSummaryData();
+
+        // wrangle data for facilityMap.js
+        wrangleFacilityMap();
+
+        // Make the ghg factor dictionary.
         GHGdata.forEach(function (d) {
             ghg[d.FY] = d["GHG factor"];
-        });
-
-        // We decided to drop the Chelmsford data.
-        plants = plants.filter(function(d){ return d.Facility!="Chelmsford Water District" });
-
-        // SavingsUSD and SavingsKWh are derived from the Usage data, and there
-        // seem to be errors.  I suggest that we drop the versions in the CSV and
-        // just generate new versions, as needed.
-        //
-        plants.forEach(function(d) {
-            delete d.SavingsUSD;
-            delete d.SavingsKWh;
-        });
-
-        // Computing GHG emissions from primary data, rather than using
-        // the values in the excel sheets.
-        plants.forEach(function(d) {
-            if(d.ElectricityGenerationKWh != ""){
-                d.GHG = (+d.ElectricityGenerationKWh)*ghg[d.FY];
-            }else{
-                d.GHG = "";
-            }
-        });
-
-        // Regularizing the cost rates
-        plants.forEach(function(d) {
-
-            // There are a few missing UsageUSD values;
-            // fill them in where possible.
-            if(d.UsageUSD == "" && d.UsageKWh != "" && d.USDperKWh != ""){
-                d.UsageUSD = (+d.UsageKWh) * (+d.USDperKWh);
-            }
-
-            if(d.USDperKWh != ""){
-                // If there as a rate in the csv file, use it.
-                d.Rate = (+d.USDperKWh);
-            }else if(d.UsageUSD != "" && d.UsageKWh != ""){
-                // If the rate is missing in the csv file but we
-                // have the usage in USD and KWh, compute the rate
-                d.Rate = (+d.UsageUSD)/(+d.UsageKWh);
-            }else {
-                // Otherwise, just assume a defaultUSDperKWh;
-                d.Rate = defaultUSDperKWh;
-            }
-        });
-
-        console.log(plants);
-        var nested = d3.nest()
-            .key(function(d) { return d.Facility;})
-            .entries(plants);
-
-        nested.forEach(function(d, index){
-
-            SummaryData[index] = {};
-            SummaryData[index].savings_USD_sum = 0.0;
-            SummaryData[index].ghg_sum = 0.0;
-            SummaryData[index].num_years = 0;
-            SummaryData[index].key = d.key;
-            SummaryData[index].energy_sum = 0.0;
-            SummaryData[index].usage_sum = 0.0;
-
-            d.values.forEach(function(data, i2){
-
-                if(data.ElectricityGenerationKWh != "" && (+data.FY)>=10){
-
-                    SummaryData[index].ghg_sum += data.GHG*metricTonsPerLb;
-                    SummaryData[index].energy_sum += (+data.ElectricityGenerationKWh);
-                    SummaryData[index].usage_sum  += (+data.UsageKWh);
-                    SummaryData[index].savings_USD_sum += (+data.ElectricityGenerationKWh)*(+data.Rate);
-                    SummaryData[index].num_years  += 1;
-
-                }
-
-            });
-
-        });
-
-        var SavingsKWh = 0;
-        var SavingsUSD = 0;
-        var SavingsGHG = 0;
-
-        SummaryData.forEach(function(data, index){
-            SavingsKWh += data.energy_sum;
-            SavingsUSD += data.savings_USD_sum;
-            SavingsGHG += data.ghg_sum;
-        });
-        console.log("Savings (Millions of KWH): ", SavingsKWh/1e6, "Savings (Millions USD): ", SavingsUSD/1e6, "Savings (Tons): ", SavingsGHG);
-
-        console.log(SummaryData);
-
-        // numify "plants" data
-        plants.forEach(function (d) {
-            d.ElectricityGenerationKWh = +d.ElectricityGenerationKWh;
-            d.GHGlbs = +d.GHGlbs;
-            d.USDperKWh = +d.USDperKWh;
-            d.UsageKWh = +d.UsageKWh;
-            d.UsageUSD = +d.UsageUSD;
-        });
-
-        // numify facilityLocations (for waterMap.js)
-        facilityLocations.forEach(function (d) {
-            d.Latitude = +d.Latitude;
-            d.Longitude = +d.Longitude;
-            d["Towns served"] = d["Towns served"].split(',');
         });
 
         // create visualizations
@@ -156,11 +73,109 @@ function wrangleData(error, regionsServed, massCities, plantsData, GHGdata) {
     }
 }
 
-// INSTANTIATE VISUALIZATIONS ------------------------------------------------------------
+// wrangle "plants" dataset
+function wranglePlants() {
+    plants.forEach(function (d) {
+        d.ElectricityGenerationKWh = +d.ElectricityGenerationKWh;
+        d.GHGlbs = +d.GHGlbs;
+        d.USDperKWh = +d.USDperKWh;
+        d.UsageKWh = +d.UsageKWh;
+        d.UsageUSD = +d.UsageUSD;
+    });
 
-// create visualizations
-function createVis() {
-    facilityMap = new FacilityMap("facility-map", facilityLocations, citiesMA, centerOfMA);
-    co2Savings = new co2Savings("co2-Savings", GHGsum);
-    usageCostScatter = new UsageCostScatter("usagecost-scatter", plants);
+    // We decided to drop the Chelmsford data.
+    plants = plants.filter(function(d){ return d.Facility!="Chelmsford Water District" });
+
+    // SavingsUSD and SavingsKWh are derived from the Usage data, and there
+    // seem to be errors.  I suggest that we drop the versions in the CSV and
+    // just generate new versions, as needed.
+    //
+    plants.forEach(function(d) {
+        delete d.SavingsUSD;
+        delete d.SavingsKWh;
+    });
+
+    // Computing GHG emissions from primary data, rather than using
+    // the values in the excel sheets.
+    plants.forEach(function(d) {
+        if(d.ElectricityGenerationKWh != ""){
+            d.GHG = (+d.ElectricityGenerationKWh)*ghg[d.FY];
+        }else{
+            d.GHG = "";
+        }
+    });
+
+    // Regularizing the cost rates
+    plants.forEach(function(d) {
+
+        // There are a few missing UsageUSD values;
+        // fill them in where possible.
+        if(d.UsageUSD == "" && d.UsageKWh != "" && d.USDperKWh != ""){
+            d.UsageUSD = (+d.UsageKWh) * (+d.USDperKWh);
+        }
+
+        if(d.USDperKWh != ""){
+            // If there as a rate in the csv file, use it.
+            d.Rate = (+d.USDperKWh);
+        }else if(d.UsageUSD != "" && d.UsageKWh != ""){
+            // If the rate is missing in the csv file but we
+            // have the usage in USD and KWh, compute the rate
+            d.Rate = (+d.UsageUSD)/(+d.UsageKWh);
+        }else {
+            // Otherwise, just assume a defaultUSDperKWh;
+            d.Rate = defaultUSDperKWh;
+        }
+    });
+}
+
+// wrangle "SummaryData" dataset
+function wrangleSummaryData() {
+    console.log(plants);
+    var nested = d3.nest()
+        .key(function(d) { return d.Facility;})
+        .entries(plants);
+
+    nested.forEach(function(d, index){
+        SummaryData[index] = {};
+        SummaryData[index].savings_USD_sum = 0.0;
+        SummaryData[index].ghg_sum = 0.0;
+        SummaryData[index].num_years = 0;
+        SummaryData[index].key = d.key;
+        SummaryData[index].energy_sum = 0.0;
+        SummaryData[index].usage_sum = 0.0;
+
+        d.values.forEach(function(data, i2){
+
+            if(data.ElectricityGenerationKWh != "" && (+data.FY)>=10){
+
+                SummaryData[index].ghg_sum += data.GHG*metricTonsPerLb;
+                SummaryData[index].energy_sum += (+data.ElectricityGenerationKWh);
+                SummaryData[index].usage_sum  += (+data.UsageKWh);
+                SummaryData[index].savings_USD_sum += (+data.ElectricityGenerationKWh)*(+data.Rate);
+                SummaryData[index].num_years  += 1;
+            }
+        });
+    });
+
+    var SavingsKWh = 0;
+    var SavingsUSD = 0;
+    var SavingsGHG = 0;
+
+    SummaryData.forEach(function(data, index){
+        SavingsKWh += data.energy_sum;
+        SavingsUSD += data.savings_USD_sum;
+        SavingsGHG += data.ghg_sum;
+    });
+    console.log("Savings (Millions of KWH): ", SavingsKWh/1e6, "Savings (Millions USD): ", SavingsUSD/1e6, "Savings (Tons): ", SavingsGHG);
+
+    console.log(SummaryData);
+}
+
+// wrangle data for facilityMap.js
+function wrangleFacilityMap() {
+    facilityLocations.forEach(function (d) {
+        d.Latitude = +d.Latitude;
+        d.Longitude = +d.Longitude;
+        d["Towns served"] = d["Towns served"].split(',');
+    });
 }
