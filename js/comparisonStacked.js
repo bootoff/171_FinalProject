e0112d47ccb8e0782a960b37343b8078824c34ae
muelle12 <jmueller@gsd.harvard.edu>
2016-11-30 22:05:21 -0500

    y axis updates

--- js/comparisonStacked.js
+++ js/comparisonStacked.js
@@ -2,9 +2,6 @@
  * Created by Janina on 11/24/2016.
  */
 
-// TO DO:
-// X-AXIS LABELS need to be rotated!
-
 Co2Savings = function(_parentElement, _data){
     this.parentElement = _parentElement;
     this.data = _data;
@@ -14,6 +11,7 @@ Co2Savings = function(_parentElement, _data){
     this.initVis();
 };
 
+//Initialization:
 
 Co2Savings.prototype.initVis = function() {
     var vis = this;
@@ -36,27 +34,28 @@ Co2Savings.prototype.initVis = function() {
     // Scales and axes
     vis.x = d3.scale.ordinal()
         .rangeRoundBands([0, vis.width], 0.2);
-        //.domain(vis.displayData.map(function (d) {return d.key; } ));
 
     vis.y = d3.scale.linear()
         .range([vis.height, 0]);
-
-    // vis.z = d3.scaleOrdinal()
-    //     .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-    //
-    // vis.stack = d3.stack();
 
     vis.xAxis = d3.svg.axis()
         .scale(vis.x)
         .orient("bottom");
 
-    vis.yAxis = d3.svg.axis()
-        .scale(vis.y)
-        .orient("left")
-        .ticks(10);
+    //THIS IS NOT WORKING:
+    vis.AxisGroup = vis.svg.append("g")
+        .attr("class", "x-axis group")
+        .attr("transform", "translate(0," + vis.height + ")rotate(-65)")
+        .call(vis.xAxis);
 
+    vis.svg.append("text")
+        .attr("class", "label x-label")
+        .attr("x", vis.width/2)
+        .attr("y", vis.height + vis.offset)
+        .style("text-anchor", "end")
+        .text("Plant Facilities");
 
-    //tool tip
+    //Tool tip
     vis.tip = d3.tip()
         .attr('class', 'd3-tip')
         .offset([-10, 0])
@@ -67,44 +66,6 @@ Co2Savings.prototype.initVis = function() {
         });
 
     vis.svg.call(vis.tip);
-
-    vis.svg.append("g")
-         //.attr("class", "x-axis axis")
-         //.attr("transform", "translate(0," + vis.height + ")")
-        //.attr("transform", "rotate(-65)")
-    //     .call(vis.xAxis)
-        // .selectAll("text")
-        //     .style("text-anchor", "end")
-        //     .attr("transform", "rotate(-65)");
-
-    vis.svg.append("g")
-        .attr("class", "y-axis axis")
-        .attr("transform", "translate(0," + vis.width + ")")
-        .call(vis.yAxis);
-
-    //TITLE, Y-AXIS AND LABELS: PUT at the end of this
-    vis.xAxisGroup = vis.svg.select(".x-axis")
-        .attr("transform", "translate(0," + vis.height + ")")
-        .call(vis.xAxis);
-
-    vis.yAxisGroup = vis.svg.select(".y-axis")
-        .call(vis.yAxis);
-
-    vis.svg.select("text.axis-title").remove();
-
-    vis.svg.append("text")
-        .attr("class", "label x-label")
-        .attr("x", vis.width/2)
-        .attr("y", vis.height + vis.offset)
-        .style("text-anchor", "end")
-         .text("Plant Facilities");
-
-    vis.svg.append("text")
-        .attr("class", "label y-label")
-        .attr("x", -(vis.height / 2))
-        .attr("y", -(vis.offset * 1.5))
-        .attr("transform", "rotate(-90)");
-        //.text("CO2 Emissions"); // this needs to be updated when selection is updated
 
     vis.wrangleData();
 }
@@ -118,15 +79,10 @@ Co2Savings.prototype.initVis = function() {
 Co2Savings.prototype.wrangleData = function() {
     var vis = this;
 
-    //console.log(vis.tip);
-
     vis.displayData = vis.data;
     vis.svg.call(vis.tip);
 
-    //console.log(vis.displayData);  //unsorted
-
     vis.category = d3.select("#ranking-type").property("value");
-    //console.log(vis.category);
 
     vis.displayData.sort(function (x, y) {
         return y[vis.category] - x[vis.category]
@@ -141,49 +97,48 @@ Co2Savings.prototype.wrangleData = function() {
             return b[selection] - a[selection]
         });
 
-        //console.log(vis.displayData); //sorted
-
         vis.updateVisualization()
     });
     vis.updateVisualization()
 }
 
 
-// Render visualization
+// Render/Update visualization
 
 Co2Savings.prototype.updateVisualization = function() {
 
         var vis = this;
 
-        //console.log(vis.tip);
         vis.displayData = vis.data;
 
         vis.svg.call(vis.tip);
 
-        //console.log(vis.displayData); //sorted
-
-        vis.y.domain([0, d3.max(vis.displayData, function (d) {
+        vis.y.domain([0, d3.extent(vis.displayData, function (d) {
         return d[vis.category];
     }) ]);
 
-    // vis.displayData.sort(function(a,b) {
-    //     return a
-    // })
-
         var selection = d3.select("#ranking-type").property("value");
-        //console.log("THIS IS THE " + selection);
 
         vis.x.domain(vis.displayData.map(function (d) {
-           // return d[selection];
             return d.key;
             }
         ));
-
 
         vis.y.domain([0, d3.max(vis.displayData, function (d) {
             return d[selection];
         })
         ]);
+
+        vis.yAxis = d3.svg.axis()
+            .scale(vis.y)
+            .orient("left")
+            .ticks(12);
+
+        vis.yAxisGroup = vis.svg.append("g")
+            .attr("class", "y axis group")
+            .call(vis.yAxis);
+
+        vis.svg.select("g").remove();
 
         vis.svg.selectAll(".bar")
             .data(vis.displayData)
@@ -203,18 +158,10 @@ Co2Savings.prototype.updateVisualization = function() {
             })
             .on('mouseover', vis.tip.show)
             .on('mouseout', function(d, index) { vis.tip.hide(d)});
-    ;
 
-
-        vis.svg.select(".y-axis")
-            .call(vis.yAxis);
-
-        vis.svg.select(".x-axis")
-            .call(vis.xAxis);
 
         var rect = vis.svg.selectAll("rect")
             .data(vis.displayData);
-
 
         rect.enter().append("rect")
             .attr("class", "bar")
@@ -222,7 +169,6 @@ Co2Savings.prototype.updateVisualization = function() {
             .on('mouseout', function(d, index) { vis.tip.hide(d)});
 
         rect.exit().remove();
-
 
         rect
             .transition()
@@ -242,4 +188,3 @@ Co2Savings.prototype.updateVisualization = function() {
 
 
     }
-
