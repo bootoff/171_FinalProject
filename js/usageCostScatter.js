5a9cfd80e35dbd6d19ff483415304f5eb79f0f20
Alma Lafler <alafler@college.harvard.edu>
2016-12-08 14:09:34 -0500

    major changes to usageCostScatter.js

--- js/usageCostScatter.js
+++ js/usageCostScatter.js
@@ -37,7 +37,7 @@ UsageCostScatter.prototype.initVis = function() {
         .append("g")
         .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top +")");
 
-    // initialize tooltip
+    // initialize tooltips
     vis.tip = d3.tip()
         .attr("class", "d3-tip")
         .offset([-10,0])
@@ -83,47 +83,13 @@ UsageCostScatter.prototype.wrangleData = function() {
     var usageCostMax = d3.max(vis.displayData, function(d) {
         return d.UsageUSD;
     });
-    // var yearExtent = [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016];
     vis.x = d3.scale.linear()
         .domain([0, usageKWhMax])
         .range([0, vis.width]);
     vis.y = d3.scale.linear()
         .domain([0, (usageCostMax + 100000)])
         .range([vis.height, 0]);
-    /*vis.colorScale = d3.scale.category10()
-        .domain(yearExtent);*/
-    vis.colors = [
-        ['#f7fcfd','#e5f5f9','#ccece6','#99d8c9','#66c2a4'], //0
-        ['#41ae76','#238b45','#006d2c','#00441b','#00200D'],
-        ['#f7fcfd','#e0ecf4','#bfd3e6','#9ebcda','#8c96c6','#8c6bb1','#88419d','#810f7c','#4d004b','#2F002E'],
-        ['#f7fcf0','#e0f3db','#ccebc5','#a8ddb5','#7bccc4','#4eb3d3'],
-        ['#2b8cbe','#0868ac','#084081','#062B55','#062B55','#062B55'], //4
-        ['#fff7ec','#fee8c8','#fdd49e','#fdbb84','#fc8d59','#ef6548','#d7301f','#b30000','#7f0000','#520000'],
-        ['#fff7fb','#ece7f2','#d0d1e6','#a6bddb','#74a9cf','#3690c0','#0570b0','#045a8d','#023858','#012134'],
-        ['#ffffff','#f0f0f0','#d9d9d9','#bdbdbd','#969696','#737373','#525252','#252525','#161616','#000000'],
-        ['#f7f4f9','#e7e1ef','#d4b9da','#c994c7','#df65b0','#e7298a','#ce1256','#980043','#67001f','#3E0013'],
-        ['#fff7f3','#fde0dd','#fcc5c0','#fa9fb5','#f768a1'], //9
-        ['#dd3497','#ae017e','#7a0177','#49006a','#000000'],
-        ['#ffffd9','#edf8b1','#c7e9b4','#7fcdbb','#41b6c4','#1d91c0'],
-        ['#225ea8','#253494','#081d58','#ffffff','#ffffff'],
-        ['#ffffe5','#f7fcb9','#d9f0a3','#addd8e','#78c679','#41ab5d','#238443','#006837','#004529','#ffffff'],
-        ['#ffffe5','#fff7bc','#fee391','#fec44f','#fe9929','#ec7014','#cc4c02','#993404','#662506','#ffffff'], //14
-        ['#f7fbff','#deebf7','#c6dbef','#9ecae1','#6baed6','#4292c6'],
-        ['#fff5eb','#fee6ce','#fdd0a2','#fdae6b','#fd8d3c','#f16913','#d94801','#a63603','#7f2704','#ffffff'],
-        ['#fcfbfd','#efedf5','#dadaeb','#bcbddc','#9e9ac8','#807dba','#6a51a3','#54278f','#3f007d','#ffffff'],
-        ['#f7fcf5','#e5f5e0','#c7e9c0','#a1d99b','#74c476','#41ab5d','#238b45','#006d2c','#00441b','#ffffff'],
-        ['#f7fcf5','#e5f5e0','#c7e9c0','#a1d99b','#74c476','#41ab5d','#238b45','#006d2c','#00441b','#ffffff']  //19
-    ];
-
-    // enumerate the facilities in vis.data (for use with vis.colors)
-    var counter = 0;
-    vis.dataRoll.forEach(function(d, i) {
-        d.values.forEach(function() {
-            //console.log(i);
-            vis.data[counter].facilityNum = i;
-            counter++;
-        })
-    });
+    vis.colors = ['#f7fcfd','#e5f5f9','#ccece6','#99d8c9','#66c2a4','#41ae76','#238b45','#006d2c','#00441b', '#000000'];
 
     // axis functions
     vis.xAxis = d3.svg.axis()
@@ -139,69 +105,19 @@ UsageCostScatter.prototype.wrangleData = function() {
 
 
 /*
- *  The drawing function
+ *  The vis update function
  */
 
 UsageCostScatter.prototype.updateVis = function() {
     var vis = this;
 
-    // line-drawing function
-    vis.drawLine = d3.svg.line()
-        .x(function(d) {
-            return vis.x(d.UsageKWh);
-        })
-        .y(function(d) {
-            return vis.y(d.UsageUSD);
-        })
-        .interpolate("linear");
-
-    var facility = vis.svg.selectAll(".facility")
-        .data(vis.dataRoll)
-        .enter()
-        .append("g")
-        .attr("class", function(d) {
-            return "facility facility-" + vis.spaceFormat(d.id);
-        })
-        .style("display", "none");
-
-    // draw (hidden) lines
-    facility.append("path")
-        .attr("class", "line")
-        .transition()
-        .attr("d", function(d) {
-            return vis.drawLine(d.values);
-        });
-
-    // draw data points
-    vis.svg.selectAll(".point")
-        .data(vis.displayData)
-        .enter()
-        .append("circle")
-        .attr("class", "point")
-        .attr("cx", function(d) {
-            return vis.x(d.UsageKWh);
-        })
-        .attr("cy", function(d) {
-            return vis.y(d.UsageUSD);
-        })
-        .attr("r", 5)
-        .style("fill", function(d) {
-            // return vis.colorScale(d.FY);
-            return vis.colors[d.facilityNum][(d.FY - 7)];
-        })
-        .attr("data-legend",function(d) {
-            return "20" + d.FY;
-        })
-        .on("mouseover", function(d) {
-            vis.tip.show(d);
-            vis.svg.selectAll(".facility-" + vis.spaceFormat(d.Facility))
-                .style("display", null);
-        })
-        .on("mouseout", function(d) {
-            vis.tip.hide(d);
-            vis.svg.selectAll(".facility-" + vis.spaceFormat(d.Facility))
-                .style("display", "none");
-        });
+    // draw line graph || draw data points
+    vis.svg.selectAll(".line")
+        .remove();
+    vis.dataRoll.forEach(function(d) {
+        vis.updateLine(d);
+        vis.updatePoints(d);
+    });
 
     // draw axes
     vis.svg.append("g")
@@ -217,7 +133,92 @@ UsageCostScatter.prototype.updateVis = function() {
         .attr("transform","translate(50,30)")
         .style("font-size","12px")
         .call(d3.legend);
+};
 
+
+/*
+ *  The line-drawing function
+ */
+
+UsageCostScatter.prototype.updateLine = function(indexData) {
+    var vis = this;
+
+    // line-drawing function
+    var drawLine = d3.svg.line()
+        .x(function(d) {
+            return vis.x(d.UsageKWh);
+        })
+        .y(function(d) {
+            return vis.y(d.UsageUSD);
+        })
+        .interpolate("linear");
+
+    // select
+    var facility = vis.svg.selectAll(".line" + vis.spaceFormat(indexData.id))
+        .data(indexData.values);
+
+    // enter & update
+    // on hover: make points for this line appear
+    facility.enter()
+        .append("path")
+        .attr("class", "line vis-line line-" + vis.spaceFormat(indexData.id))
+        .on("mouseover", function() {
+            vis.svg.selectAll(".point-" + vis.spaceFormat(indexData.id))
+                .style("display", null);
+        })
+        .on("mouseout", function() {
+            vis.svg.selectAll(".point-" + vis.spaceFormat(indexData.id))
+                .style("display", "none");
+        })
+        .transition()
+        .delay(700)
+        .duration(800)
+        .attr("d", drawLine(indexData.values));
+
+    // exit
+    facility.exit()
+        .remove();
+};
+
+/*
+ *  The point-drawing function
+ */
+
+UsageCostScatter.prototype.updatePoints = function(indexData) {
+    var vis = this;
+
+    // draw data points (hidden)
+    // on hover: make tooltip appear & make all points for this line appear
+    vis.svg.selectAll(".point" + vis.spaceFormat(indexData.id))
+        .data(indexData.values)
+        .enter()
+        .append("circle")
+        .attr("class", "point vis-point point-" + vis.spaceFormat(indexData.id))
+        .attr("cx", function(d) {
+            return vis.x(d.UsageKWh);
+        })
+        .attr("cy", function(d) {
+            return vis.y(d.UsageUSD);
+        })
+        .attr("r", 5)
+        .style("fill", function(d) {
+            return vis.colors[d.FY - 7];
+        })
+        .style("display", "none")
+        .attr("data-legend",function(d) {
+            return "20" + d.FY;
+        })
+        .on("mouseover", function(d) {
+            console.log(d);
+            vis.tip.show(d);
+            vis.svg.selectAll(".point-" + vis.spaceFormat(indexData.id))
+                .style("display", null);
+        })
+        .on("mouseout", function(d) {
+            vis.tip.hide(d);
+            vis.svg.selectAll(".point-" + vis.spaceFormat(indexData.id))
+                .style("display", "none");
+        });
 };
 
 
